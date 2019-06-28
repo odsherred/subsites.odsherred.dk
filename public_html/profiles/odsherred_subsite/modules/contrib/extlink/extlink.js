e2836e54987cd672c9d8fa1e7a35e83ffd890e7c
juuliabellcom <juuliadevadze@gmail.com>
2019-06-28 22:03:33 +0300

    Field Slideshow 7.x-1.81 -> 7.x-1.82

--- public_html/profiles/odsherred_subsite/modules/contrib/extlink/extlink.js
+++ public_html/profiles/odsherred_subsite/modules/contrib/extlink/extlink.js
@@ -1,106 +1,204 @@
-// $Id: extlink.js,v 1.8 2010/05/26 01:25:56 quicksketch Exp $
+/**
+ * @file
+ */
+
 (function ($) {
 
-function extlinkAttach(context) {
-  // Strip the host name down, removing ports, subdomains, or www.
-  var pattern = /^(([^\/:]+?\.)*)([^\.:]{4,})((\.[a-z]{1,4})*)(:[0-9]{1,5})?$/;
-  var host = window.location.host.replace(pattern, '$3$4');
-  var subdomain = window.location.host.replace(pattern, '$1');
+  'use strict';
 
-  // Determine what subdomains are considered internal.
-  if (Drupal.settings.extlink.extSubdomains) {
-    var subdomains = "([^/]*\\.)?";
-  }
-  else if (subdomain == 'www.' || subdomain == '') {
-    var subdomains = "(www\\.)?";
-  }
-  else {
-    var subdomains = subdomain.replace(".", "\\.");
-  }
+  Drupal.extlink = Drupal.extlink || {};
 
-  // Build regular expressions that define an internal link.
-  var internal_link = new RegExp("^https?://" + subdomains + host, "i");
-
-  // Extra internal link matching.
-  var extInclude = false;
-  if (Drupal.settings.extlink.extInclude) {
-    extInclude = new RegExp(Drupal.settings.extlink.extInclude.replace(/\\/, '\\'));
-  }
-
-  // Extra external link matching.
-  var extExclude = false;
-  if (Drupal.settings.extlink.extExclude) {
-    extExclude = new RegExp(Drupal.settings.extlink.extExclude.replace(/\\/, '\\'));
-  }
-
-  // Find all links which are NOT internal and begin with http (as opposed
-  // to ftp://, javascript:, etc. other kinds of links.
-  // When operating on the 'this' variable, the host has been appended to
-  // all links by the browser, even local ones.
-  // In jQuery 1.1 and higher, we'd use a filter method here, but it is not
-  // available in jQuery 1.0 (Drupal 5 default).
-  var external_links = new Array();
-  var mailto_links = new Array();
-  $("a:not(." + Drupal.settings.extlink.extClass + ", ." + Drupal.settings.extlink.mailtoClass + ")", context).each(function(el) {
-    try {
-      var url = this.href.toLowerCase();
-      if (url.indexOf('http') == 0 && (!url.match(internal_link) || (extInclude && url.match(extInclude))) && !(extExclude && url.match(extExclude))) {
-        external_links.push(this);
-      }
-      else if (url.indexOf('mailto:') == 0) {
-        mailto_links.push(this);
-      }
+  Drupal.extlink.attach = function (context, settings) {
+    if (!settings.hasOwnProperty('extlink')) {
+      return;
     }
-    // IE7 throws errors often when dealing with irregular links, such as:
-    // <a href="node/10"></a> Empty tags.
-    // <a href="http://user:pass@example.com">example</a> User:pass syntax.
-    catch(error) {
-      return false;
-    }
-  });
 
-  if (Drupal.settings.extlink.extClass) {
-    // Apply the "ext" class to all links not containing images.
-    if (parseFloat($().jquery) < 1.2) {
-      $(external_links).not('[img]').addClass(Drupal.settings.extlink.extClass).each(function() { if ($(this).css('display') == 'inline') $(this).after('<span class=' + Drupal.settings.extlink.extClass + '></span>'); });
+    // Strip the host name down, removing ports, subdomains, or www.
+    var pattern = /^(([^\/:]+?\.)*)([^\.:]{1,})((\.[a-z0-9]{1,253})*)(:[0-9]{1,5})?$/;
+    var host = window.location.host.replace(pattern, '$2$3');
+    var subdomain = window.location.host.replace(host, '');
+
+    // Determine what subdomains are considered internal.
+    var subdomains;
+    if (settings.extlink.extSubdomains) {
+      subdomains = '([^/]*\\.)?';
+    }
+    else if (subdomain === 'www.' || subdomain === '') {
+      subdomains = '(www\\.)?';
     }
     else {
-      $(external_links).not($(external_links).find('img').parents('a')).addClass(Drupal.settings.extlink.extClass).each(function() { if ($(this).css('display') == 'inline') $(this).after('<span class=' + Drupal.settings.extlink.extClass + '></span>'); });
+      subdomains = subdomain.replace('.', '\\.');
     }
-  }
 
-  if (Drupal.settings.extlink.mailtoClass) {
-    // Apply the "mailto" class to all mailto links not containing images.
-    if (parseFloat($().jquery) < 1.2) {
-      $(mailto_links).not('[img]').addClass(Drupal.settings.extlink.mailtoClass).each(function() { if ($(this).css('display') == 'inline') $(this).after('<span class=' + Drupal.settings.extlink.mailtoClass + '></span>'); });
+    // Build regular expressions that define an internal link.
+    var internal_link = new RegExp('^https?://([^@]*@)?' + subdomains + host, 'i');
+
+    // Extra internal link matching.
+    var extInclude = false;
+    if (settings.extlink.extInclude) {
+      extInclude = new RegExp(settings.extlink.extInclude.replace(/\\/, '\\'), 'i');
     }
-    else {
-      $(mailto_links).not($(mailto_links).find('img').parents('a')).addClass(Drupal.settings.extlink.mailtoClass).each(function() { if ($(this).css('display') == 'inline') $(this).after('<span class=' + Drupal.settings.extlink.mailtoClass + '></span>'); });
+
+    // Extra external link matching.
+    var extExclude = false;
+    if (settings.extlink.extExclude) {
+      extExclude = new RegExp(settings.extlink.extExclude.replace(/\\/, '\\'), 'i');
     }
-  }
 
-  if (Drupal.settings.extlink.extTarget) {
-    // Apply the target attribute to all links.
-    $(external_links).attr('target', Drupal.settings.extlink.extTarget);
-  }
+    // Extra external link CSS selector exclusion.
+    var extCssExclude = false;
+    if (settings.extlink.extCssExclude) {
+      extCssExclude = settings.extlink.extCssExclude;
+    }
 
-  if (Drupal.settings.extlink.extAlert) {
-    // Add pop-up click-through dialog.
-    $(external_links).click(function(e) {
-     return confirm(Drupal.settings.extlink.extAlertText);
+    // Extra external link CSS selector explicit.
+    var extCssExplicit = false;
+    if (settings.extlink.extCssExplicit) {
+      extCssExplicit = settings.extlink.extCssExplicit;
+    }
+
+    // Define the jQuery method (either 'append' or 'prepend') of placing the icon, defaults to 'append'.
+    var extIconPlacement = settings.extlink.extIconPlacement || 'append';
+
+    // Find all links which are NOT internal and begin with http as opposed
+    // to ftp://, javascript:, etc. other kinds of links.
+    // When operating on the 'this' variable, the host has been appended to
+    // all links by the browser, even local ones.
+    // In jQuery 1.1 and higher, we'd use a filter method here, but it is not
+    // available in jQuery 1.0 (Drupal 5 default).
+    var external_links = [];
+    var mailto_links = [];
+    $('a:not(.' + settings.extlink.extClass + ', .' + settings.extlink.mailtoClass + '), area:not(.' + settings.extlink.extClass + ', .' + settings.extlink.mailtoClass + ')', context).each(function (el) {
+      try {
+        var url = '';
+        if (typeof this.href == 'string') {
+          url = this.href.toLowerCase();
+        }
+        // Handle SVG links (xlink:href).
+        else if (typeof this.href == 'object') {
+          url = this.href.baseVal;
+        }
+        if (url.indexOf('http') === 0
+          && ((!url.match(internal_link) && !(extExclude && url.match(extExclude))) || (extInclude && url.match(extInclude)))
+          && !(extCssExclude && $(this).is(extCssExclude))
+          && !(extCssExclude && $(this).parents(extCssExclude).length > 0)
+          && !(extCssExplicit && $(this).parents(extCssExplicit).length < 1)) {
+          external_links.push(this);
+        }
+        // Do not include area tags with begin with mailto: (this prohibits
+        // icons from being added to image-maps).
+        else if (this.tagName !== 'AREA'
+          && url.indexOf('mailto:') === 0
+          && !(extCssExclude && $(this).parents(extCssExclude).length > 0)
+          && !(extCssExplicit && $(this).parents(extCssExplicit).length < 1)) {
+          mailto_links.push(this);
+        }
+      }
+      // IE7 throws errors often when dealing with irregular links, such as:
+      // <a href="node/10"></a> Empty tags.
+      // <a href="http://user:pass@example.com">example</a> User:pass syntax.
+      catch (error) {
+        return false;
+      }
     });
-  }
 
-  // Work around for Internet Explorer box model problems.
-  if (($.support && !($.support.boxModel === undefined) && !$.support.boxModel) || ($.browser.msie && parseInt($.browser.version) <= 7)) {
-    $('span.ext, span.mailto').css('display', 'inline-block');
-  }
-}
+    if (settings.extlink.extClass) {
+      Drupal.extlink.applyClassAndSpan(external_links, settings.extlink.extClass, extIconPlacement);
+    }
 
-Drupal.behaviors.extlink = {
-  attach: function(context){
-    extlinkAttach(context);
-  }
-}
+    if (settings.extlink.mailtoClass) {
+      Drupal.extlink.applyClassAndSpan(mailto_links, settings.extlink.mailtoClass, extIconPlacement);
+    }
+
+    if (settings.extlink.extTarget) {
+      // Apply the target attribute to all links.
+      $(external_links).attr('target', settings.extlink.extTarget);
+      // Add rel attributes noopener and noreferrer.
+      $(external_links).attr('rel', function (i, val) {
+        // If no rel attribute is present, create one with the values noopener and noreferrer.
+        if (val == null) {
+          return 'noopener noreferrer';
+        }
+        // Check to see if rel contains noopener or noreferrer. Add what doesn't exist.
+        if (val.indexOf('noopener') > -1 || val.indexOf('noreferrer') > -1) {
+          if (val.indexOf('noopener') === -1) {
+            return val + ' noopener';
+          }
+          if (val.indexOf('noreferrer') === -1) {
+            return val + ' noreferrer';
+          }
+          // Both noopener and noreferrer exist. Nothing needs to be added.
+          else {
+            return val;
+          }
+        }
+        // Else, append noopener and noreferrer to val.
+        else {
+          return val + ' noopener noreferrer';
+        }
+      });
+    }
+
+    Drupal.extlink = Drupal.extlink || {};
+
+    // Set up default click function for the external links popup. This should be
+    // overridden by modules wanting to alter the popup.
+    Drupal.extlink.popupClickHandler = Drupal.extlink.popupClickHandler || function () {
+      if (settings.extlink.extAlert) {
+        return confirm(settings.extlink.extAlertText);
+      }
+    };
+
+    $(external_links).click(function (e) {
+      return Drupal.extlink.popupClickHandler(e, this);
+    });
+  };
+
+  /**
+   * Apply a class and a trailing <span> to all links not containing images.
+   *
+   * @param {object[]} links
+   *   An array of DOM elements representing the links.
+   * @param {string} class_name
+   *   The class to apply to the links.
+   * @param {string} icon_placement
+   *   'append' or 'prepend' the icon to the link.
+   */
+  Drupal.extlink.applyClassAndSpan = function (links, class_name, icon_placement) {
+    var $links_to_process;
+    if (Drupal.settings.extlink.extImgClass) {
+      $links_to_process = $(links);
+    }
+    else {
+      var links_with_images = $(links).find('img').parents('a');
+      $links_to_process = $(links).not(links_with_images);
+    }
+    $links_to_process.addClass(class_name);
+    var i;
+    var length = $links_to_process.length;
+    for (i = 0; i < length; i++) {
+      var $link = $($links_to_process[i]);
+      if ($link.css('display') === 'inline' || $link.css('display') === 'inline-block') {
+        if (class_name === Drupal.settings.extlink.mailtoClass) {
+          $link[icon_placement]('<span class="' + class_name + '" aria-label="' + Drupal.settings.extlink.mailtoLabel + '"></span>');
+        }
+        else {
+          $link[icon_placement]('<span class="' + class_name + '" aria-label="' + Drupal.settings.extlink.extLabel + '"></span>');
+        }
+      }
+    }
+  };
+
+  Drupal.behaviors.extlink = Drupal.behaviors.extlink || {};
+  Drupal.behaviors.extlink.attach = function (context, settings) {
+    // Backwards compatibility, for the benefit of modules overriding extlink
+    // functionality by defining an "extlinkAttach" global function.
+    if (typeof extlinkAttach === 'function') {
+      extlinkAttach(context);
+    }
+    else {
+      Drupal.extlink.attach(context, settings);
+    }
+  };
 
 })(jQuery);
